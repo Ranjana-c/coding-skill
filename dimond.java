712b7a986fea7ddefff45906c17c2e5582a82403
Ranjanadevi-c <146353171+Ranjana-c@users.noreply.github.com>
2024-07-18 10:34:18 +0530

    Create dimond.java
    dimond code

---
+++ dimond.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Diamond {
+    public static void main(String[] args) {
+        int n = 5; 
+        printDiamond(n);
+    }
+
+    public static void printDiamond(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
