380bfc9d78848072302c1a511db71a9ab83e4e5e
Ranjanadevi-c <146353171+Ranjana-c@users.noreply.github.com>
2024-07-18 19:03:36 +0530

    Create palindrom.java
    palindrom

---
+++ palindrom.java
@@ -0,0 +1,68 @@
+import java.util.*;
+class DLLNode {
+    char data;
+    DLLNode prev, next;
+
+    DLLNode(char d) {
+        data = d;
+        prev = next = null;
+    }
+}
+
+class DLList {
+    DLLNode head, tail;
+
+    DLList() {
+        head = tail = null;
+    }
+
+    void add(char data) {
+        DLLNode newNode = new DLLNode(data);
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+    }
+
+    boolean isPalindrome() {
+        if (head == null) {
+            return true; 
+        }
+
+        DLLNode left = head;
+        DLLNode right = tail;
+
+        while (left != null && right != null) {
+            if (left.data != right.data) {
+                return false;
+            }
+            left = left.next;
+            right = right.prev;
+        }
+
+        return true;
+    }
+}
+
+public class PalindromeUsingDLL {
+
+    public static void main(String[] args) {
+        String str = "madam";
+        
+        DLList dll = new DLList();
+        
+       
+        for (int i = 0; i < str.length(); i++) {
+            dll.add(str.charAt(i));
+        }
+        
+        if (dll.isPalindrome()) {
+            System.out.println(str + " is a palindrome.");
+        } else {
+            System.out.println(str + " is not a palindrome.");
+        }
+    }
+}
